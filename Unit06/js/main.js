86b0e214019a09ca04d69188f2c7e22b25e905b4
Jake <jakeacarter20@gmail.com>
2021-03-24 10:46:23 -0600

    display block on the textareas and added color to story

--- Unit06/js/main.js
+++ Unit06/js/main.js
@@ -1,10 +1,12 @@
 function tellStory() {
     const storyElement = document.querySelector('#story')
+    
 
     let nounsString = document.querySelector('#noun').value
     nounArray = nounsString.trim().split(' ');
     nounArray = nounArray.flatMap(noun => noun.split('\n'))
     nounArray = nounArray.map(noun => noun.toLowerCase())
+    
 
     let adjString = document.querySelector('#adjectives').value
     adjArray = adjString.trim().split(' ');
@@ -21,7 +23,7 @@ function tellStory() {
     
  console.log(verbArray)  
  
- const myStory = `Once upon a time there was a ${adjArray.shift()} ${nounArray.shift()}`
+ const myStory = `Once upon a time there was a ${adjArray.shift()} ${nounArray.shift()} that loved to ${verbArray.shift()} and ${verbArray.shift()} over all of the ${nounArray.shift()}s. His name was Bernard, and he lived in an ${adjArray.shift()} ${nounArray.shift()} that had hundreds of ${nounArray.shift()}s roaming the area. He had a friend that was a ${nounArray.shift()}, and his name was Fred. Fred loved to scavenge for ${adjArray.shift()} ${nounArray.shift()}s. After many hours of scavengeing, Fred could not find anything and so he convinced Bernard to leave, so they ran away, riding thier favorite ${nounArray.shift()}.`
  storyElement.innerHTML = myStory
  
 console.log(myStory)
