2c6ccc80923cc75ad1e3ba9a30fbe57765634db4
Jake <jakeacarter20@gmail.com>
2021-03-23 21:17:00 -0600

    created  arrays for everything and started creating story

--- Unit06/js/main.js
+++ Unit06/js/main.js
@@ -1,11 +1,31 @@
 function tellStory() {
+    const storyElement = document.querySelector('#story')
+
+    let nounsString = document.querySelector('#noun').value
+    nounArray = nounsString.trim().split(' ');
+    nounArray = nounArray.flatMap(noun => noun.split('\n'))
+    nounArray = nounArray.map(noun => noun.toLowerCase())
+
+    let adjString = document.querySelector('#adjectives').value
+    adjArray = adjString.trim().split(' ');
+    adjArray = adjArray.flatMap(adjective => adjective.split('\n'))
+    adjArray = adjArray.map(adj => adj.toLowerCase())
+
+    const verbString = document.querySelector('#verbs').value
+    verbArray = verbString.trim().split(' ');
+    verbArray = verbArray.flatMap(verb => verb.split('\n'))
+    verbArray = verbArray.map(verb => verb.toLowerCase())
+
+
     //let nounArray = ['#noun'];
-    let nounArray = document.querySelector('#noun')
-
-    let adjectiveArray = ['#adjectives'];
     
-    let verbArray = ['#verbs'];
+ console.log(verbArray)  
+ 
+ const myStory = `Once upon a time there was a ${adjArray.shift()} ${nounArray.shift()}`
+storyElement.innerHTML = myStory
+ 
+console.log(myStory)
 
-   
+ 
 
 }
