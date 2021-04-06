703b9219d87aba17bc67af80dc65d1179ca19297
Jake <jakeacarter20@gmail.com>
2021-04-06 16:57:54 -0600

    create structure and design started duplicate menu

---
+++ Unit09/js/main.js
@@ -0,0 +1,16 @@
+function duplicateMenu() {
+    let topList = document.querySelectorAll('ul#primaryNavigation li a')
+
+    let newList = document.createElement('ul')
+
+    topList.forEach(menuItem => {
+        let newLI = document.createElement('li')
+        let newLink = document.createElement('a')
+        newLink.setAttribute('href', menuItem.getAttribute('href'))
+        // 'copy' the textContent from upper menu to new menu
+
+        //append children to make them apear in the DOM
+    })
+}
+
+duplicateMenu()
